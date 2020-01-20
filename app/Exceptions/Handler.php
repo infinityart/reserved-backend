eecd1fe3340ac18a0af1fdbb037e97c6cbbd163f
Artwolf <jonty_wolf@hotmail.com>
2020-01-20 08:18:28 +0100

    Added general httpException handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,11 +3,15 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -39,12 +43,40 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     * @param \Illuminate\Http\Request $request
+     * @param Exception $e
+     * @return \Illuminate\Http\JsonResponse|Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e)
     {
-        return parent::render($request, $exception);
+        if (env('APP_DEBUG')) {
+            return parent::render($request, $e);
+        }
+
+        $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        if ($e instanceof HttpResponseException) {
+            $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+        } elseif ($e instanceof MethodNotAllowedHttpException) {
+            $status = Response::HTTP_METHOD_NOT_ALLOWED;
+            $e = new MethodNotAllowedHttpException([], 'HTTP_METHOD_NOT_ALLOWED', $e);
+        } elseif ($e instanceof NotFoundHttpException) {
+            $status = Response::HTTP_NOT_FOUND;
+            $e = new NotFoundHttpException('HTTP_NOT_FOUND', $e);
+        } elseif ($e instanceof AuthorizationException) {
+            $status = Response::HTTP_FORBIDDEN;
+            $e = new AuthorizationException('HTTP_FORBIDDEN', $status);
+        } elseif ($e instanceof \Dotenv\Exception\ValidationException && $e->getResponse()) {
+            $status = Response::HTTP_BAD_REQUEST;
+            $e = new \Dotenv\Exception\ValidationException('HTTP_BAD_REQUEST', $status, $e);
+        } elseif ($e) {
+            $e = new HttpException($status, 'HTTP_INTERNAL_SERVER_ERROR');
+        }
+
+        return response()->json([
+            'success' => false,
+            'status' => $status,
+            'message' => $e->getMessage()
+        ], $status);
     }
 }
