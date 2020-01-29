82c93d737c655ac8ca479398a3f9b4754e2d973a
Jonty <jonty_wolf@hotmail.com>
2020-01-29 20:26:03 +0100

    Fixed relationship in hairdresser

--- app/Http/Controllers/HairdresserController.php
+++ app/Http/Controllers/HairdresserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Appointment;
 use App\Hairdresser;
 use App\Http\Resources\HairdresserAppointmentsResource;
 use App\Http\Resources\HairdresserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
@@ -31,15 +33,15 @@ class HairdresserController extends Controller
         $date = $validatedData['date'];
         $date = Carbon::createFromFormat('d-m-Y', $date);
 
-        $hairdressers = Hairdresser::whereHas('appointments', function ($query) use ($date) {
+        $hairdressers = Hairdresser::with(['appointments' => function ($query) use ($date) {
             $query->whereDate('ScheduledAt', '=', $date->format('Y-m-d'));
-        })->get();
+        }])->has('appointments')->get();
 
         foreach ($hairdressers as $hairdresser) {
             foreach ($hairdresser->appointments as $appointment) {
                 $endTime = $appointment->ScheduledAt;
-
                 foreach ($appointment->treatments as $treatment) {
+
                     $duration = Carbon::createFromFormat('H:i:s', $treatment->Duration);
 
                     $endTime->addHours($duration->hour);
