44b6f3a0cf28c66ef6203261e40adc4c1c5ad302
Jonty <jonty_wolf@hotmail.com>
2020-01-29 20:41:05 +0100

    Added value to request response

--- app/Http/Resources/HairdresserAppointmentsResource.php
+++ app/Http/Resources/HairdresserAppointmentsResource.php
@@ -14,6 +14,7 @@ class HairdresserAppointmentsResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->ID,
             'name' => $this->Name,
             'appointments' => [
                 AppointmentResource::collection($this->appointments)
